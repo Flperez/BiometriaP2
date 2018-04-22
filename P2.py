7b46f37f114420223b35c507dc4b3b8e14e7cb4f
f <felipeperez2710@gmail.com>
2018-04-22 18:55:24 +0200

    Quitado los extremos y codigo organizado

--- P2.py
+++ P2.py
@@ -1,132 +1,50 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-
-from skimage.morphology import skeletonize, remove_small_objects
-from skimage import data
-import matplotlib.pyplot as plt
-from skimage.util import invert
-
-def getMax(cnt):
-    cnt = np.reshape(cnt,(-1,2))
-    return np.array([np.max(cnt[:,1]),np.max(cnt[:,0])],dtype=np.int)
+import argparse
+from tools import *
+import os
+from natsort import natsorted
 
 
-def getMin(cnt):
-    cnt = np.reshape(cnt,(-1,2))
-    return np.array([np.min(cnt[:,1]),np.min(cnt[:,0])],dtype=np.int)
+def main(input,output,visu=False):
+    names = natsorted(os.listdir(input))
+    for name in names:
+        # 1.- Cargamos las imagenes
+        img, img_invert, img_logical = load_image(os.path.join(input,name))
 
-def findLimts(skel):
-    limitPoint = np.ones(skel.shape,dtype=bool)
+        # 2.- Esqueletizamos la huella
+        skel_logical, skel_image = calcSkel(img_logical)
 
-    # TODO: Buscar los puntos donde no se aplicara la busqueda de los puntos de terminacion y de bifurcacion
-    #
-    # contours = cv2.findContours(skel, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # for cnts in contours:
-    #     if type(cnts) == list:
-    #         for cnt in cnts:
-    #             limitPoint[getMax(cnt)] = False
-    #             limitPoint[getMin(cnt)] = False
-    #     else:
-    #         cnt = cnts
-    #         limitPoint[getMax(cnt)] = False
-    #         limitPoint[getMin(cnt)] = False
-    return limitPoint
+        # 3.- Buscamos los puntos en los que no deberiamos aplicar la busqueda de minucias
+        mask = findLimts(skel_image)
 
-def findKeypoints(skel,limitPoint):
-    endPoint = []
-    forkPoint = []
-    whitepoint = np.asarray(np.where(skel==255)).T
-    limitPoint = np.asarray(limitPoint)
-    # [whitepoint[0][0], whitepoint[1][0]
-    for xy in whitepoint:
-        # TODO: si pertenece xy a los limites no usar
-        # if not np.any(limitPoint==xy):
-        num = np.count_nonzero(skel[xy[0]-1:xy[0]+2,xy[1]-1:xy[1]+2])-1
-        if num==1:
-            endPoint.append(xy)
-        if num > 2:
-            forkPoint.append(xy)
+        # 4.- Buscamos los puntos finales y de bifurcacion
+        endPoint, forkPoint = findKeypoints(skel_image,mask)
 
-    return endPoint,forkPoint
 
-def drawPoint(image,lst_point,color):
-    out = image.copy()
-    for xy in lst_point:
-        out[xy[0],xy[1],:] = np.asarray(color)
-    return out
+
+        if visu:
+            # plot2images(img_invert,img_logical)
+            # plot2images(img_logical,skel_logical)
+            # plot2images(skel_logical,skel_image)
+            out = cv2.cvtColor(img_invert.copy(),cv2.COLOR_GRAY2RGB)
+            # out = drawPoint(out, endPoint, (255, 0, 0))
+            # out = drawPoint(out, forkPoint, (0, 255, 0))
+
+            out = drawCircle(out, endPoint, (255, 0, 0))
+            out = drawCircle(out, forkPoint, (0, 255, 0))
+
+            plot2images(skel_logical,out)
+
 
 if __name__=="__main__":
-    img = cv2.imread('huellasFVC04/102_3.tif', 0)
-    img = 255*np.ones(img.shape)-img
-    img = img.astype(np.uint8)
-    img_logical = np.logical_and(img>100,img>101)
-    original = img.copy()
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--input", required=True,
+                    help="ruta a la carpeta de entrada de imagenes")
+    ap.add_argument("--output", required=False,
+                    help="ruta a la carpeta de salida de imagenes")
+    args = vars(ap.parse_args())
+    input = args['input']
+    output = args['output']
+    main(input,output,True)
 
 
 
-    # Invert the horse image
-
-
-    skeleton = skeletonize(img_logical)
-    # TODO: small_objects
-    # skeleton = remove_small_objects(skeleton)
-
-    # display results
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 4),
-                             sharex=True, sharey=True)
-
-    ax = axes.ravel()
-
-    skel = 255*skeleton.astype(np.uint8)
-    ax[0].imshow(img, cmap=plt.cm.gray)
-    ax[0].axis('off')
-    ax[0].set_title('cv2 ', fontsize=20)
-
-    ax[1].imshow(skel, cmap=plt.cm.gray)
-    ax[1].axis('off')
-    ax[1].set_title('skeleton', fontsize=20)
-
-    fig.tight_layout()
-    plt.show()
-
-    limitPoint = findLimts(skel)
-    endPoint,forkPoint = findKeypoints(skel,limitPoint)
-    out = cv2.cvtColor(skel,cv2.COLOR_GRAY2RGB)
-    #
-    out = drawPoint(out,endPoint,(255,0,0))
-    out = drawPoint(out,forkPoint,(0,255,0))
-    # out = drawPoint(out,limitPoint,(0,0,255))
-
-
-    # plt.hist(original.ravel(),256,[0,256]); plt.show()
-
-    # plt.imshow(skel,cmap="gray"); plt.show()
-
-    # display results
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 4),
-                             sharex=True, sharey=True)
-
-    ax = axes.ravel()
-
-    ax[0].imshow(skel, cmap=plt.cm.gray)
-    ax[0].axis('off')
-    ax[0].set_title('skeleton ', fontsize=20)
-
-    ax[1].imshow(out)
-    ax[1].axis('off')
-    ax[1].set_title('limits', fontsize=20)
-
-
-    fig.tight_layout()
-    plt.show()
-
-    cv2.imshow("out",out)
-    cv2.waitKey()
-
-
-
-    # cv2.imshow("skel", skel)
-    # cv2.imshow("original", original)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
---
+++ tools.py
@@ -0,0 +1,105 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+from skimage.morphology import skeletonize, remove_small_objects
+
+
+
+########## Paso 1 ##########
+def load_image(path):
+    # Load image
+    img = cv2.imread(path, 0)
+
+    # invert image
+    img_invert = 255 * np.ones(img.shape) - img
+    img_invert = img_invert.astype(np.uint8)
+
+    # Logical image
+    blur = cv2.GaussianBlur(img, (5, 5), 0)
+    th3 =  cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    img_logical = 255 * np.ones(img.shape) - th3
+    img_logical = np.logical_and(img_logical > 170, img_logical > 171)
+    return img,img_invert,img_logical
+
+
+########## Paso 2 ##########
+def calcSkel(img_logical):
+    skel_logical = skeletonize(img_logical)
+    skel_image = 255 * skel_logical.astype(np.uint8)
+    return skel_logical, skel_image
+
+
+########## Paso 3 ##########
+def findLimts(skel):
+    mask = np.ones(skel.shape,dtype=bool)
+    # Buscamos el punto min y max de cada fila
+    whitepoint = np.asarray(np.where(skel==255)).T
+    fil_unique,indexes = np.unique(whitepoint[:,0],return_index=True)
+    lst_limits = np.array([],dtype=np.int)
+    for x in range(indexes.shape[0]-1):
+        points = whitepoint[indexes[x]:indexes[x+1],:]
+        maximum = np.max(points[:,1])
+        minimum = np.min(points[:,1])
+        lst_limits = np.append(lst_limits,np.array([fil_unique[x],maximum]))
+        lst_limits = np.append(lst_limits,np.array([fil_unique[x],minimum]))
+    lst_limits = np.reshape(lst_limits,(-1,2))
+    for limit in lst_limits:
+        mask[limit[0], limit[1]]=False
+
+    return mask
+
+########## Paso 4 ##########
+
+def findKeypoints(skel,mask):
+    endPoint = []
+    forkPoint = []
+    whitepoint = np.asarray(np.where(skel==255)).T
+    for xy in whitepoint:
+        if mask[xy[0], xy[1]]:
+            num = np.count_nonzero(skel[xy[0]-1:xy[0]+2,xy[1]-1:xy[1]+2])-1
+            if num==1:
+                endPoint.append(xy)
+            if num > 2:
+                forkPoint.append(xy)
+
+    return endPoint,forkPoint
+
+
+def drawPoint(image,lst_point,color):
+    out = image.copy()
+    for xy in lst_point:
+        out[xy[0],xy[1],:] = np.asarray(color)
+    return out
+
+def drawCircle(image,lst_point,color):
+    out = image.copy()
+    for xy in lst_point:
+        cv2.circle(out,(xy[1],xy[0]),1,color,1)
+    return out
+
+
+
+
+#### Plot fuction #######
+def plot2images(img1,img2,title1='img1',title2='img2'):
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 4),
+                             sharex=True, sharey=True)
+    ax = axes.ravel()
+
+    ax[0].imshow(img1, cmap=plt.cm.gray)
+    ax[0].axis('off')
+    ax[0].set_title(title1, fontsize=20)
+
+    ax[1].imshow(img2, cmap=plt.cm.gray)
+    ax[1].axis('off')
+    ax[1].set_title(title2, fontsize=20)
+
+    fig.tight_layout()
+    plt.show()
+
+def plot4images(images,titles):
+    for i in range(4):
+        plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+        plt.title(titles[i])
+        plt.xticks([]),plt.yticks([])
+    plt.show()
